8ecc9c49a947c1045ed6c7d2d5ff7ac2b1289bd0
Taki / <amrito@myyahoo.com>
2024-03-11 10:50:06 +0530

    sm6375-common: Project Zephyrus: Adapt for updated `hardware/xiaomi` path

--- holi.mk
+++ holi.mk
@@ -525,7 +525,7 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/xiaomi
+    vendor/hardware/xiaomi
 
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
